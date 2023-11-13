4b33b025888bde595e31fd45a986e00c15414c10
sharper <hi@sammyharper.com>
2023-11-13 12:59:48 -0600

    dev: update localserve micro-opt version

---
+++ localserve-python_v2.0.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+:: v2.0
+:: to execute as expected, requires python 3.x
+
+:start
+set choice=
+set /p choice= Open browser to view served website? [N]: 
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='Y' goto yes
+if '%choice%'=='y' goto yes
+if '%choice%'=='N' goto no
+IF '%choice%'=='n' goto no
+if '%choice%'=='' goto no
+echo "%choice%" is not valid
+echo.
+goto start
+
+:no
+echo Skipped...
+clear
+GOTO
+
+:yes
+echo Opening browser...
+start "" http://localhost:8000
+
+:startServer
+ECHO starting local server at
+ECHO %cd%
+ECHO with python (localhost:8000)
+python -m http.server
